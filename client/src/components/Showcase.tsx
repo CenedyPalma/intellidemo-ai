bdc0319a11508baab834adf7abf5009a71611eee
cofficeup <cofficeup@gmail.com>
2026-01-19 00:17:33 +0600

    Remove unused imports to fix Render build

--- client/src/components/Showcase.tsx
+++ client/src/components/Showcase.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { Float } from '@react-three/drei';
 import { motion } from 'framer-motion';
